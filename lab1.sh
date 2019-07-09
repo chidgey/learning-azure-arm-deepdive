93782f30c371e977add71e135d99bea149219de9
Adam Chidgey <40761319+chidgey@users.noreply.github.com>
2019-07-09 11:59:32 +0100

    Running example with params injected

--- lab1.sh
+++ lab1.sh
@@ -1,4 +1,12 @@
 
-cd lab1
-az group create --name lab1 --location "uk south"
-az group deployment create --name job1 --resource-group lab1 --template-file azuredeploy.json
+#cd lab1
+#az group create --name lab1 --location "uk south"
+#az group deployment create --name job1 --resource-group lab1 --template-file azuredeploy.json
+
+# Inject parameters from the shell
+rg=lab1
+template=/d/projects/learning-azure-arm-deepdive/lab1/azuredeploy.json
+job=job2
+params="StorageAccountParam=\"cclabstorageac001\""
+
+echo az group deployment create --name $job --resource-group $rg --template-file $template --parameters $params
